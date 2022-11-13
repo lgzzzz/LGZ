f76728f13fe473d906ed7de14cc841d7b86c11eb
LGZ <903416023@qq.com>
2022-11-14 01:09:36 +0800

    newpassword

--- pkg/newpassword.go
+++ pkg/newpassword.go
@@ -1,17 +1,29 @@
 package pkg
 
 import (
-	"encoding/hex"
 	"math/rand"
 	"time"
 )
 
-var NewPassportName = "newpassport"
-
 func NewPassport() string {
 	p := make([]byte, 10)
 	rand.Seed(time.Now().Unix())
-	rand.Read(p)
-	str := hex.EncodeToString(p)
-	return str
+	for i := range p {
+		for {
+			ch := rand.Intn(255)
+			if ch >= int('a') && ch <= int('z') {
+				p[i] = byte(ch)
+				break
+			}
+			if ch >= int('A') && ch <= int('Z') {
+				p[i] = byte(ch)
+				break
+			}
+			if ch >= int('0') && ch <= int('9') {
+				p[i] = byte(ch)
+				break
+			}
+		}
+	}
+	return string(p)
 }
